3b4fe7750f1dc1d386fe11e23acc94310d184f73
niw5924 <niw5924@naver.com>
2025-03-10 03:36:45 +0900

    feat: 알람성공화면 시간대별 메세지 설정

--- lib/alarm/alarm_success_screen.dart
+++ lib/alarm/alarm_success_screen.dart
@@ -9,7 +9,47 @@ class AlarmSuccessScreen extends StatefulWidget {
 }
 
 class AlarmSuccessScreenState extends State<AlarmSuccessScreen> {
-  String displayText = '좋은 아침!';
+  late String displayText;
+
+  @override
+  void initState() {
+    super.initState();
+    displayText = getGreetingMessage(); // 초기 메시지 설정
+  }
+
+  String getGreetingMessage() {
+    final hour = DateTime.now().hour;
+
+    switch (hour) {
+      case >= 6 && < 12:
+        return "좋은 아침!";
+      case >= 12 && < 18:
+        return "좋은 오후예요!";
+      case >= 18 && < 24:
+        return "좋은 저녁이에요!";
+      case >= 0 && < 6:
+        return "늦은 시간까지 고생했어요!";
+      default:
+        return "시간 정보 오류"; // 예외 처리용
+    }
+  }
+
+  String getEncouragingMessage() {
+    final hour = DateTime.now().hour;
+
+    switch (hour) {
+      case >= 6 && < 12:
+        return "오늘 하루도 화이팅!";
+      case >= 12 && < 18:
+        return "남은 하루도 힘내세요!";
+      case >= 18 && < 24:
+        return "편안한 밤 되세요!";
+      case >= 0 && < 6:
+        return "충분한 휴식을 취하세요!";
+      default:
+        return "시간 정보 오류"; // 예외 처리용
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,22 +64,20 @@ class AlarmSuccessScreenState extends State<AlarmSuccessScreen> {
               height: 250,
               child: Lottie.asset(
                 'assets/animation/lottie_check.json',
-                repeat: false, // 애니메이션 반복 안 함
+                repeat: false,
                 onLoaded: (composition) {
                   final halfDuration = composition.duration.inMilliseconds ~/ 2;
 
-                  // 첫 번째 절반: "좋은 아침!" 표시
                   Future.delayed(Duration(milliseconds: halfDuration), () {
                     if (mounted) {
                       setState(() {
-                        displayText = '오늘 하루도 화이팅!';
+                        displayText = getEncouragingMessage(); // 두 번째 메시지 설정
                       });
                     }
                   });
 
-                  // 전체 애니메이션 시간 후 화면 닫기
                   Future.delayed(composition.duration, () {
-                    if (mounted) Navigator.pop(context);
+                    if (context.mounted) Navigator.pop(context);
                   });
                 },
               ),
